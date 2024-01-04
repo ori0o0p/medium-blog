91d9dbf822d980420a1e43b8881184f8e2de91e1
101 <tmddnjs001133@dsm.hs.kr>
2024-01-04 11:01:36 +0900

    엔드포인트 추가

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/board/controller/BoardController.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.example.mediumspringwebflux.board.dto.BoardRequest;
 import com.example.mediumspringwebflux.board.dto.BoardResponse;
 import com.example.mediumspringwebflux.board.service.CreateBoard;
 import com.example.mediumspringwebflux.board.service.FindAllBoard;
+import com.example.mediumspringwebflux.board.service.FindByIdBoard;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -13,10 +14,12 @@ import reactor.core.publisher.Mono;
 public class BoardController {
     private final CreateBoard createBoard;
     private final FindAllBoard findAllBoard;
+    private final FindByIdBoard findByIdBoard;
 
-    public BoardController(CreateBoard createBoard, FindAllBoard findAllBoard) {
+    public BoardController(CreateBoard createBoard, FindAllBoard findAllBoard, FindByIdBoard findByIdBoard) {
         this.createBoard = createBoard;
         this.findAllBoard = findAllBoard;
+        this.findByIdBoard = findByIdBoard;
     }
 
     @PostMapping
@@ -27,6 +30,11 @@ public class BoardController {
     @GetMapping
     public Flux<BoardResponse> findAll() {
         return findAllBoard.execute();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<BoardResponse> findById(@PathVariable String id) {
+        return findByIdBoard.execute(id);
     }
 
 }
