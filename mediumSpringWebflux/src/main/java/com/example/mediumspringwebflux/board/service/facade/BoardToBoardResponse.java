e85dbda30f7e25c6c9cf364c793102e808aaed35
101 <tmddnjs001133@dsm.hs.kr>
2024-01-08 19:59:23 +0900

    .

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/board/service/facade/BoardToBoardResponse.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/board/service/facade/BoardToBoardResponse.java
@@ -2,18 +2,26 @@ package com.example.mediumspringwebflux.board.service.facade;
 
 import com.example.mediumspringwebflux.board.document.Board;
 import com.example.mediumspringwebflux.board.dto.BoardResponse;
+import com.example.mediumspringwebflux.comment.collection.CommentCollection;
+import com.example.mediumspringwebflux.comment.dto.CommentResponse;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
 public class BoardToBoardResponse {
 
     public Mono<BoardResponse> rapping(Board board) {
-        return Mono.fromCallable(() -> BoardResponse.builder()
-                .id(board.getId())
-                .title(board.getTitle())
-                .description(board.getDescription())
-                .build());
+        CommentCollection commentCollection = new CommentCollection(board.getComments());
+        Flux<CommentResponse> commentListFlux = commentCollection.toCommentResponse();
+
+        return commentListFlux.collectList()
+                .map(commentList -> BoardResponse.builder()
+                        .id(board.getId())
+                        .title(board.getTitle())
+                        .description(board.getDescription())
+                        .comments(commentList)
+                        .build());
     }
 
 }
