33546da77ab90e8270fe1f1ee2d5d559de331c73
101 <tmddnjs001133@dsm.hs.kr>
2024-01-08 19:59:15 +0900

    dto수정

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/board/dto/BoardResponse.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/board/dto/BoardResponse.java
@@ -1,11 +1,15 @@
 package com.example.mediumspringwebflux.board.dto;
 
+import com.example.mediumspringwebflux.comment.dto.CommentResponse;
 import lombok.Builder;
+
+import java.util.List;
 
 @Builder
 public record BoardResponse(
         String id,
         String title,
-        String description
+        String description,
+        List<CommentResponse> comments
 ) {
 }
