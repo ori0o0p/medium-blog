7ee98ea9398d747a5aeb5c63f564076787702b72
101 <tmddnjs001133@dsm.hs.kr>
2024-01-12 14:47:21 +0900

    생성 시간 추가

--- mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/document/Board.kt
+++ mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/document/Board.kt
@@ -3,11 +3,13 @@ package com.example.mediumspringwebfluxcorutine.board.document
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
 
 @Document
 data class Board(
     @Id
     val id: ObjectId = ObjectId.get(),
     val title: String,
-    val content: String
+    val content: String,
+    val createdDate: LocalDateTime
 )
