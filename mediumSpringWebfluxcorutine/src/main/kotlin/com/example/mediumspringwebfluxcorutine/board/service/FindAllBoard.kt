9023f6aca432f7e63ddba41a60e64e3b83c985e4
101 <tmddnjs001133@dsm.hs.kr>
2024-01-12 14:48:05 +0900

    게시물 전체 조회 추가

--- mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/service/FindAllBoard.kt
+++ mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/service/FindAllBoard.kt
@@ -1,13 +1,28 @@
 package com.example.mediumspringwebfluxcorutine.board.service
 
+import com.example.mediumspringwebfluxcorutine.board.document.Board
+import com.example.mediumspringwebfluxcorutine.board.dto.BoardResponse
 import com.example.mediumspringwebfluxcorutine.board.repository.BoardRepository
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.reactive.asFlow
 
 class FindAllBoard(
     private val boardRepository: BoardRepository
 ) {
 
-    suspend fun execute() {
+    suspend fun execute(): List<BoardResponse> {
+        val boards: List<Board> = boardRepository.findAll()
+            .asFlow().toList()
 
+        return boards.map { b -> warp(b) }
+    }
+
+    private fun warp(board: Board): BoardResponse {
+        return BoardResponse(
+            id = board.id.toString(),
+            title = board.title,
+            content = board.content
+        )
     }
 
 }
