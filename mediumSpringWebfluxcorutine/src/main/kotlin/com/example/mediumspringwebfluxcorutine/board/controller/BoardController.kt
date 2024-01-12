cce8c52f534f36dbbb6aa1ba3b2df13a2f6e80cb
101 <tmddnjs001133@dsm.hs.kr>
2024-01-12 14:55:45 +0900

    엔드포인트 추가

--- mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/controller/BoardController.kt
+++ mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/controller/BoardController.kt
@@ -1,7 +1,10 @@
 package com.example.mediumspringwebfluxcorutine.board.controller
 
 import com.example.mediumspringwebfluxcorutine.board.dto.BoardRequest
+import com.example.mediumspringwebfluxcorutine.board.dto.BoardResponse
 import com.example.mediumspringwebfluxcorutine.board.service.CreateBoard
+import com.example.mediumspringwebfluxcorutine.board.service.FindAllBoard
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,9 +13,14 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/boards")
 class BoardController(
-    private val createBoard: CreateBoard
+    private val createBoard: CreateBoard,
+    private val findAllBoard: FindAllBoard
 ) {
 
+    @GetMapping
+    suspend fun findAll(): List<BoardResponse> {
+        return findAllBoard.execute()
+    }
 
     @PostMapping
     suspend fun create(@RequestBody boardRequest: BoardRequest) {
