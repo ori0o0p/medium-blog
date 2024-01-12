ef14c872ae2a34b1952e90a596ef99d554880860
101 <tmddnjs001133@dsm.hs.kr>
2024-01-12 12:23:55 +0900

    수정

--- mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/service/CreateBoard.kt
+++ mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/service/CreateBoard.kt
@@ -3,6 +3,7 @@ package com.example.mediumspringwebfluxcorutine.board.service
 import com.example.mediumspringwebfluxcorutine.board.document.Board
 import com.example.mediumspringwebfluxcorutine.board.dto.BoardRequest
 import com.example.mediumspringwebfluxcorutine.board.repository.BoardRepository
+import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,6 +15,7 @@ class CreateBoard(
         boardRepository.save(Board(
                 title = boardRequest.title,
                 content = boardRequest.content))
+            .awaitSingle()
     }
 
 }
