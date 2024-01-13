ced36a53a4f18bb16d83f72491d7fbdc8d802373
101 <tmddnjs001133@dsm.hs.kr>
2024-01-13 20:33:38 +0900

    게시물 삭제

---
+++ mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/service/DeleteBoard.kt
@@ -0,0 +1,18 @@
+package com.example.mediumspringwebfluxcorutine.board.service
+
+import com.example.mediumspringwebfluxcorutine.board.repository.BoardRepository
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class DeleteBoard(
+    private val boardRepository: BoardRepository
+) {
+
+    suspend fun execute(id: String) {
+        boardRepository.deleteById(id)
+            .awaitSingleOrNull()
+    }
+
+}
