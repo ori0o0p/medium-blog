7a96584599acff908c1bccd69b36ac38a54cddfb
101 <tmddnjs001133@dsm.hs.kr>
2024-01-13 20:33:52 +0900

    ^&^

--- mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/controller/BoardController.kt
+++ mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/controller/BoardController.kt
@@ -3,8 +3,10 @@ package com.example.mediumspringwebfluxcorutine.board.controller
 import com.example.mediumspringwebfluxcorutine.board.dto.BoardRequest
 import com.example.mediumspringwebfluxcorutine.board.dto.BoardResponse
 import com.example.mediumspringwebfluxcorutine.board.service.CreateBoard
+import com.example.mediumspringwebfluxcorutine.board.service.DeleteBoard
 import com.example.mediumspringwebfluxcorutine.board.service.FindAllBoard
 import com.example.mediumspringwebfluxcorutine.board.service.FindByIdBoard
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -17,22 +19,22 @@ import org.springframework.web.bind.annotation.RestController
 class BoardController(
     private val createBoard: CreateBoard,
     private val findAllBoard: FindAllBoard,
-    private val findByIdBoard: FindByIdBoard
+    private val findByIdBoard: FindByIdBoard,
+    private val deleteBoard: DeleteBoard
 ) {
 
     @GetMapping
-    suspend fun findAll(): List<BoardResponse> {
-        return findAllBoard.execute()
-    }
+    suspend fun findAll(): List<BoardResponse> = findAllBoard.execute()
 
     @GetMapping("/{id}")
-    suspend fun findById(@PathVariable id: String): BoardResponse {
-        return findByIdBoard.execute(id)
-    }
+    suspend fun findById(@PathVariable id: String): BoardResponse = findByIdBoard.execute(id)
+
 
     @PostMapping
-    suspend fun create(@RequestBody boardRequest: BoardRequest) {
-        createBoard.execute(boardRequest)
-    }
+    suspend fun create(@RequestBody boardRequest: BoardRequest) = createBoard.execute(boardRequest)
+
+
+    @DeleteMapping("/{id}")
+    suspend fun delete(@PathVariable id: String) = deleteBoard.execute(id)
 
 }
