58494ee68658333c6bcbc12dd8ae2f2a1d2bc652
101 <tmddnjs001133@dsm.hs.kr>
2024-01-13 20:19:55 +0900

    엔드포인트 추가

--- mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/controller/BoardController.kt
+++ mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/controller/BoardController.kt
@@ -4,7 +4,9 @@ import com.example.mediumspringwebfluxcorutine.board.dto.BoardRequest
 import com.example.mediumspringwebfluxcorutine.board.dto.BoardResponse
 import com.example.mediumspringwebfluxcorutine.board.service.CreateBoard
 import com.example.mediumspringwebfluxcorutine.board.service.FindAllBoard
+import com.example.mediumspringwebfluxcorutine.board.service.FindByIdBoard
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,12 +16,18 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/boards")
 class BoardController(
     private val createBoard: CreateBoard,
-    private val findAllBoard: FindAllBoard
+    private val findAllBoard: FindAllBoard,
+    private val findByIdBoard: FindByIdBoard
 ) {
 
     @GetMapping
     suspend fun findAll(): List<BoardResponse> {
         return findAllBoard.execute()
+    }
+
+    @GetMapping("/{id}")
+    suspend fun findById(@PathVariable id: String): BoardResponse {
+        return findByIdBoard.execute(id)
     }
 
     @PostMapping
