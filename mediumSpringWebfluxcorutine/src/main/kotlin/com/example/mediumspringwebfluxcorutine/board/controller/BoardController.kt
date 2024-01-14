e679d34975f23a067abf25a48d050c56a4d79b34
101 <tmddnjs001133@dsm.hs.kr>
2024-01-14 14:02:20 +0900

    엔드포인트 추가

--- mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/controller/BoardController.kt
+++ mediumSpringWebfluxcorutine/src/main/kotlin/com/example/mediumspringwebfluxcorutine/board/controller/BoardController.kt
@@ -2,12 +2,10 @@ package com.example.mediumspringwebfluxcorutine.board.controller
 
 import com.example.mediumspringwebfluxcorutine.board.dto.BoardRequest
 import com.example.mediumspringwebfluxcorutine.board.dto.BoardResponse
-import com.example.mediumspringwebfluxcorutine.board.service.CreateBoard
-import com.example.mediumspringwebfluxcorutine.board.service.DeleteBoard
-import com.example.mediumspringwebfluxcorutine.board.service.FindAllBoard
-import com.example.mediumspringwebfluxcorutine.board.service.FindByIdBoard
+import com.example.mediumspringwebfluxcorutine.board.service.*
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -20,6 +18,7 @@ class BoardController(
     private val createBoard: CreateBoard,
     private val findAllBoard: FindAllBoard,
     private val findByIdBoard: FindByIdBoard,
+    private val updateBoard: UpdateBoard,
     private val deleteBoard: DeleteBoard
 ) {
 
@@ -33,6 +32,9 @@ class BoardController(
     @PostMapping
     suspend fun create(@RequestBody boardRequest: BoardRequest) = createBoard.execute(boardRequest)
 
+    @PatchMapping("/{id}")
+    suspend fun update(@PathVariable id: String, @RequestBody boardRequest: BoardRequest)
+    = updateBoard.execute(id, boardRequest)
 
     @DeleteMapping("/{id}")
     suspend fun delete(@PathVariable id: String) = deleteBoard.execute(id)
