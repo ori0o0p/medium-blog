74b27415c5b93576a8defdbef4f7d1da39abcf5a
101 <tmddnjs001133@dsm.hs.kr>
2024-01-15 10:43:25 +0900

    테스트 추가

--- mediumSpringWebClientExternalAPI/src/test/java/com/example/mediumspringwebclientexternalapi/MediumSpringWebClientExternalApiApplicationTests.java
+++ mediumSpringWebClientExternalAPI/src/test/java/com/example/mediumspringwebclientexternalapi/MediumSpringWebClientExternalApiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.mediumspringwebclientexternalapi;
 
+import com.example.mediumspringwebclientexternalapi.api.WebClientService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MediumSpringWebClientExternalApiApplicationTests {
 
+	@Autowired
+	private WebClientService webClientService;
+
 	@Test
-	void contextLoads() {
+	void get() {
+		webClientService.get();
 	}
 
 }
