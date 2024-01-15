f4dc7f3c0534723677afcfbe88690fcbbcbee9ed
101 <tmddnjs001133@dsm.hs.kr>
2024-01-15 10:28:00 +0900

    프로젝트 시작

---
+++ mediumSpringWebClientExternalAPI/src/main/java/com/example/mediumspringwebclientexternalapi/MediumSpringWebClientExternalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediumspringwebclientexternalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumSpringWebClientExternalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediumSpringWebClientExternalApiApplication.class, args);
+	}
+
+}
---
+++ mediumSpringWebClientExternalAPI/src/test/java/com/example/mediumspringwebclientexternalapi/MediumSpringWebClientExternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediumspringwebclientexternalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumSpringWebClientExternalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
