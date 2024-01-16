3e9741924613ae9f49320007951bd2b987f879ae
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 17:17:14 +0900

    유저 정보 저장

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/board/service/CreateBoard.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/board/service/CreateBoard.java
@@ -3,21 +3,28 @@ package com.example.mediumspringwebflux.domain.board.service;
 import com.example.mediumspringwebflux.domain.board.dto.BoardRequest;
 import com.example.mediumspringwebflux.domain.board.document.Board;
 import com.example.mediumspringwebflux.domain.board.repository.BoardRepository;
+import com.example.mediumspringwebflux.domain.user.document.User;
+import com.example.mediumspringwebflux.domain.user.service.UserFacade;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class CreateBoard {
     private final BoardRepository boardRepository;
+    private final UserFacade userFacade;
 
-    public CreateBoard(BoardRepository boardRepository) {
+    public CreateBoard(BoardRepository boardRepository, UserFacade userFacade) {
         this.boardRepository = boardRepository;
+        this.userFacade = userFacade;
     }
 
     public Mono<Void> execute(BoardRequest request) {
-        return Mono.just(Board.builder()
+
+        return userFacade.getUser()
+                .map(user -> Board.builder()
                         .title(request.title())
                         .description(request.description())
+                        .writer(user)
                         .build())
                 .flatMap(boardRepository::save)
                 .then();
