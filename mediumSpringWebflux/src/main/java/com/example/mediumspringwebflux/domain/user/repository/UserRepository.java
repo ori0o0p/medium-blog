0e98a0eda3549f8bd535fb4e4e419d188d92a7ac
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 15:49:01 +0900

    find by email 추가

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/user/repository/UserRepository.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/user/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.example.mediumspringwebflux.domain.user.repository;
 
 import com.example.mediumspringwebflux.domain.user.document.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
+
+    Mono<User> findByEmail(String email);
 }
