be83cd20632b177574e786a8d6be14530ef3e1cd
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 17:54:35 +0900

    bean 등록

---
+++ mediumSpringQRGenerator/src/main/kotlin/com/example/mediumspringqrgenerator/qr/BufferedImageConverter.kt
@@ -0,0 +1,17 @@
+package com.example.mediumspringqrgenerator.qr
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.BufferedImageHttpMessageConverter
+import org.springframework.http.converter.HttpMessageConverter
+import java.awt.image.BufferedImage
+
+@Configuration
+class BufferedImageConverter {
+
+    @Bean
+    fun httpMessageConverter(): HttpMessageConverter<BufferedImage> {
+        return BufferedImageHttpMessageConverter()
+    }
+
+}
