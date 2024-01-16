788f11a46403c817f720abeb4174bacea68f865d
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 17:29:21 +0900

    작성자 정보 저장

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/comment/service/CreateComment.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/comment/service/CreateComment.java
@@ -5,6 +5,7 @@ import com.example.mediumspringwebflux.domain.board.service.facade.FindBoard;
 import com.example.mediumspringwebflux.domain.comment.document.Comment;
 import com.example.mediumspringwebflux.domain.comment.dto.CommentRequest;
 import com.example.mediumspringwebflux.domain.comment.repository.CommentRepository;
+import com.example.mediumspringwebflux.domain.user.service.UserFacade;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -13,28 +14,32 @@ public class CreateComment {
     private final CommentRepository commentRepository;
     private final BoardRepository boardRepository;
     private final FindBoard findBoard;
+    private final UserFacade userFacade;
 
-    public CreateComment(CommentRepository commentRepository, BoardRepository boardRepository, FindBoard findBoard) {
+    public CreateComment(CommentRepository commentRepository, BoardRepository boardRepository, FindBoard findBoard, UserFacade userFacade) {
         this.commentRepository = commentRepository;
         this.boardRepository = boardRepository;
         this.findBoard = findBoard;
+        this.userFacade = userFacade;
     }
 
     public Mono<Void> execute(CommentRequest request) {
-        return findBoard.execute(request.boardId())
-                .flatMap(board -> {
-                    Comment comment = Comment.builder()
-                            .comment(request.comment())
-                            .boardId(board.getId())
-                            .build();
+        return userFacade.getUser()
+                .flatMap(user -> findBoard.execute(request.boardId())
+                        .flatMap(board -> {
+                            Comment comment = Comment.builder()
+                                    .comment(request.comment())
+                                    .boardId(board.getId())
+                                    .writer(user)
+                                    .build();
 
-                    return commentRepository.save(comment)
-                            .then(Mono.just(board))
-                            .flatMap(b -> {
-                                b.getComments().add(comment);
-                                return boardRepository.save(b);
-                            });
-                }).then();
+                            return commentRepository.save(comment)
+                                    .then(Mono.just(board))
+                                    .flatMap(b -> {
+                                        b.getComments().add(comment);
+                                        return boardRepository.save(b);
+                                    });
+                        })).then();
     }
 
 }
