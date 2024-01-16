04033d7b0900f4d58df19e8339760b0841a4e375
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 10:25:43 +0900

    filter chain 추가

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/config/SecurityConfig.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/config/SecurityConfig.java
@@ -1,2 +1,23 @@
-package com.example.mediumspringwebflux.global.config;public class SecurityConfig {
+package com.example.mediumspringwebflux.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(exchanges -> exchanges
+                .pathMatchers().permitAll()
+                .anyExchange().authenticated())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .build();
+    }
+
 }
