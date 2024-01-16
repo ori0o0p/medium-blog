ed840deac51940f9a7c1c9710b8e2923f87bc1f5
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 10:32:29 +0900

    토큰 생성

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/security/Tokenizer.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/security/Tokenizer.java
@@ -1,2 +1,33 @@
-package com.example.mediumspringwebflux.global.security;public class Tokenizer {
+package com.example.mediumspringwebflux.global.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Component
+public class Tokenizer {
+
+    private static final String SECRET = "secret";
+
+    private String tokenize(String user, String type) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR, 2);
+        Date expiresAt = calendar.getTime();
+
+        Claims claims = Jwts.claims()
+                .setSubject(user);
+        claims.put("type", type);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuer("url")
+                .setExpiration(expiresAt)
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+
 }
