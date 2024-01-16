ed2956ca6f9225b8670a760289931ff668876747
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 17:17:24 +0900

    필요없는 코드 삭제

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/config/SecurityConfig.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
