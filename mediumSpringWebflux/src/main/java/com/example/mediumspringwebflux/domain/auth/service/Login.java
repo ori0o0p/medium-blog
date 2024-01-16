d0f69e7e11b3261903421347ce1d2697e360d361
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 16:06:26 +0900

    로그인

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/service/Login.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/service/Login.java
@@ -1,6 +1,7 @@
 package com.example.mediumspringwebflux.domain.auth.service;
 
 import com.example.mediumspringwebflux.domain.auth.dto.LoginRequest;
+import com.example.mediumspringwebflux.domain.auth.dto.LoginResponse;
 import com.example.mediumspringwebflux.domain.user.repository.UserRepository;
 import com.example.mediumspringwebflux.global.security.Tokenizer;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,8 +20,17 @@ public class Login {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public Mono<Void> execute(LoginRequest request) {
-        return Mono.empty();
+    public Mono<LoginResponse> execute(LoginRequest request) {
+        return userRepository.findByEmail(request.email())
+                .switchIfEmpty(Mono.error(new RuntimeException("유저를 찾지 못")))
+                .filter(user -> passwordEncoder.matches(request.password(), user.getPassword()))
+                .map(user -> {
+                    final String token = tokenizer.createToken(user.getEmail());
+
+                    return LoginResponse.builder()
+                            .token(token)
+                            .build();
+                }).onErrorResume(Mono::error);
     }
 
 }
