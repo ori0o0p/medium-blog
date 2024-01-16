22126bdc0761034865e07a24e9369856a9d11f50
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 15:56:04 +0900

    password encoder 추가

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/service/Login.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/service/Login.java
@@ -2,6 +2,7 @@ package com.example.mediumspringwebflux.domain.auth.service;
 
 import com.example.mediumspringwebflux.domain.user.repository.UserRepository;
 import com.example.mediumspringwebflux.global.security.Tokenizer;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -9,10 +10,12 @@ import reactor.core.publisher.Mono;
 public class Login {
     private final UserRepository userRepository;
     private final Tokenizer tokenizer;
+    private final PasswordEncoder passwordEncoder;
 
-    public Login(UserRepository userRepository, Tokenizer tokenizer) {
+    public Login(UserRepository userRepository, Tokenizer tokenizer, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.tokenizer = tokenizer;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Mono<Void> execute() {
