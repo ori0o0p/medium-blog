33355b74e014c7c18cf5f1ffeb31e709a52b0752
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 16:11:56 +0900

    uri 추가

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/controller/AuthController.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/controller/AuthController.java
@@ -6,10 +6,12 @@ import com.example.mediumspringwebflux.domain.auth.service.Login;
 import com.example.mediumspringwebflux.domain.auth.service.Signup;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
     private final Login login;
     private final Signup signup;
@@ -19,7 +21,7 @@ public class AuthController {
         this.signup = signup;
     }
 
-    @PostMapping
+    @PostMapping("/login")
     public Mono<LoginResponse> setLogin(@RequestBody LoginRequest request) {
         return login.execute(request);
     }
