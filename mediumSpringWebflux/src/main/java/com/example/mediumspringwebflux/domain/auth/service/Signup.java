9f2ef5930c8c899349a7e84ce20205ff9aef8d13
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 15:52:40 +0900

    회원가입 로직

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/service/Signup.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/service/Signup.java
@@ -1,10 +1,13 @@
 package com.example.mediumspringwebflux.domain.auth.service;
 
 import com.example.mediumspringwebflux.domain.auth.dto.SignupRequest;
+import com.example.mediumspringwebflux.domain.user.document.User;
 import com.example.mediumspringwebflux.domain.user.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
 
 @Service
 public class Signup {
@@ -20,8 +23,17 @@ public class Signup {
         return userRepository.findByEmail(request.email())
                 .flatMap(user -> Mono.error(new RuntimeException("유저가 이미 존재")))
                 .switchIfEmpty(Mono.defer(() -> {
-                    final String password =
-                }))
+                    final String password = passwordEncoder.encode(request.password());
+                    
+                    User user = User.builder()
+                            .email(request.email())
+                            .name(request.name())
+                            .password(password)
+                            .createdDate(LocalDate.now())
+                            .build();
+
+                    return userRepository.save(user);
+                })).then();
     }
 
 }
