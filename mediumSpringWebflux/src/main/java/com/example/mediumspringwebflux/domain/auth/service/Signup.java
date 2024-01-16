589dad1e3a7c62a30f7e8ca9fafab62d1b7d419d
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 15:49:56 +0900

    password encoder 추가

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/service/Signup.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/service/Signup.java
@@ -1,19 +1,27 @@
 package com.example.mediumspringwebflux.domain.auth.service;
 
+import com.example.mediumspringwebflux.domain.auth.dto.SignupRequest;
 import com.example.mediumspringwebflux.domain.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class Signup {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public Signup(UserRepository userRepository) {
+    public Signup(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public Mono<Void> execute() {
-        return Mono.empty();
+    public Mono<Void> execute(SignupRequest request) {
+        return userRepository.findByEmail(request.email())
+                .flatMap(user -> Mono.error(new RuntimeException("유저가 이미 존재")))
+                .switchIfEmpty(Mono.defer(() -> {
+                    final String password =
+                }))
     }
 
 }
