0fc3e3b325abfbe80f7e81129ed10e27e19bd595
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 14:35:33 +0900

    수정

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/service/Login.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/service/Login.java
@@ -1,15 +1,18 @@
 package com.example.mediumspringwebflux.domain.auth.service;
 
 import com.example.mediumspringwebflux.domain.user.repository.UserRepository;
+import com.example.mediumspringwebflux.global.security.Tokenizer;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class Login {
     private final UserRepository userRepository;
+    private final Tokenizer tokenizer;
 
-    public Login(UserRepository userRepository) {
+    public Login(UserRepository userRepository, Tokenizer tokenizer) {
         this.userRepository = userRepository;
+        this.tokenizer = tokenizer;
     }
 
     public Mono<Void> execute() {
