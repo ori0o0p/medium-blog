c597bb43750eb50a2bc410814dc6ede35b85d533
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 17:39:47 +0900

    프로젝트 시작!

---
+++ mediumSpringQRGenerator/src/main/kotlin/com/example/mediumspringqrgenerator/MediumSpringQrGeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mediumspringqrgenerator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MediumSpringQrGeneratorApplication
+
+fun main(args: Array<String>) {
+	runApplication<MediumSpringQrGeneratorApplication>(*args)
+}
---
+++ mediumSpringQRGenerator/src/test/kotlin/com/example/mediumspringqrgenerator/MediumSpringQrGeneratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mediumspringqrgenerator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MediumSpringQrGeneratorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
