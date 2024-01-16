8be58cdb82f7b31e4cedc3e7963d7b92412534d6
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 15:48:50 +0900

    비밀번호 인코더

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/config/SecurityConfig.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -18,6 +20,11 @@ public class SecurityConfig {
 
     public SecurityConfig(Tokenizer tokenizer) {
         this.tokenizer = tokenizer;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
