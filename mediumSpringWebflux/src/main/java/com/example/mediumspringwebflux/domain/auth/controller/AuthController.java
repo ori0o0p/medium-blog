2f07084fd4fd32e2742a8cbe62a1ebe0dbca9ee9
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 16:08:41 +0900

    추가

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/controller/AuthController.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/domain/auth/controller/AuthController.java
@@ -1,2 +1,17 @@
-package com.example.mediumspringwebflux.domain.auth.controller;public class AuthController {
+package com.example.mediumspringwebflux.domain.auth.controller;
+
+import com.example.mediumspringwebflux.domain.auth.service.Login;
+import com.example.mediumspringwebflux.domain.auth.service.Signup;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    private final Login login;
+    private final Signup signup;
+
+    public AuthController(Login login, Signup signup) {
+        this.login = login;
+        this.signup = signup;
+    }
+
 }
