d28779971628eb74133f04fc4a105ada4f353df3
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 14:22:50 +0900

    필터 적용

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/config/SecurityConfig.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.example.mediumspringwebflux.global.config;
 
+import com.example.mediumspringwebflux.global.security.JwtWebFilter;
+import com.example.mediumspringwebflux.global.security.Tokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    private final Tokenizer tokenizer;
+
+    public SecurityConfig(Tokenizer tokenizer) {
+        this.tokenizer = tokenizer;
+    }
 
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
@@ -17,6 +27,7 @@ public class SecurityConfig {
                 .anyExchange().authenticated())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .addFilterBefore(new JwtWebFilter(tokenizer), SecurityWebFiltersOrder.HTTP_BASIC)
                 .build();
     }
 
