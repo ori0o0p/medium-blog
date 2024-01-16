0c68352629a8fdfee3dd93974763337c130d1d6d
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 17:16:35 +0900

    에러 수정

--- mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/security/JwtWebFilter.java
+++ mediumSpringWebflux/src/main/java/com/example/mediumspringwebflux/global/security/JwtWebFilter.java
@@ -1,15 +1,15 @@
 package com.example.mediumspringwebflux.global.security;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-@Component
 public class JwtWebFilter implements WebFilter {
     private final Tokenizer tokenizer;
 
@@ -19,19 +19,19 @@ public class JwtWebFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        return extractToken(exchange).flatMap(token -> {
-            if (StringUtils.hasText(token) && tokenizer.verify(token)) {
-                return Mono.just(tokenizer.getAuthentication(token))
-                        .doOnNext(ReactiveSecurityContextHolder::withAuthentication)
-                        .then(chain.filter(exchange));
-            }
-            return Mono.empty();
-        }).switchIfEmpty(chain.filter(exchange));
+        String token = resolveToken(exchange.getRequest());
+        if(StringUtils.hasText(token) && tokenizer.verify(token)) {
+            Authentication authentication = tokenizer.getAuthentication(token);
+            return chain.filter(exchange)
+                    .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication));
+        }
+        return chain.filter(exchange);
+
     }
 
-    private Mono<String> extractToken(ServerWebExchange exchange) {
-        return Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION))
-                .map(s -> s.substring(7));
+    private String resolveToken(ServerHttpRequest request) {
+        return request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION)
+                .substring(7);
     }
 
 }
