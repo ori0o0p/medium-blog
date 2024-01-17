ac9e0f86ec13058ab99a51175248b8b11aa9231a
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 22:36:49 +0900

    get user 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/user/service/facade/UserFacade.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/user/service/facade/UserFacade.kt
@@ -1,4 +1,20 @@
 package com.example.mediumspringwebmvc.domain.user.service.facade
 
-class UserFacade {
+import com.example.mediumspringwebmvc.domain.user.model.User
+import com.example.mediumspringwebmvc.domain.user.repository.UserRepository
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class UserFacade(
+    private val userRepository: UserRepository
+) {
+
+    fun getUser(): User {
+        val authentication = SecurityContextHolder.getContext().authentication
+        val email = authentication.name
+
+        return userRepository.findByEmail(email) ?: throw RuntimeException("유저를 찾지 못함")
+    }
+
 }
