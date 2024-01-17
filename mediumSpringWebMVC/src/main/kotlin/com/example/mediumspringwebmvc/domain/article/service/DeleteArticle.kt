bf3d148e35a34a00fa1826391fcf2f16de3b0394
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 14:26:03 +0900

    게시물 삭제 로직 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/DeleteArticle.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/DeleteArticle.kt
@@ -1,4 +1,18 @@
 package com.example.mediumspringwebmvc.domain.article.service
 
-class DeleteArticle {
+import com.example.mediumspringwebmvc.domain.article.repository.ArticleRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class DeleteArticle(
+    private val articleRepository: ArticleRepository
+) {
+
+    fun execute(id: Long) {
+        val article = articleRepository.findByIdOrNull(id) ?: throw RuntimeException("게시물을 찾지 못함")
+
+        articleRepository.delete(article)
+    }
+
 }
