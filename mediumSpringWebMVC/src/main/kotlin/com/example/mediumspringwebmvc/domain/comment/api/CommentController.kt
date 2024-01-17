1fc65d2ae4ef6604fd1571ab9c7c4db6f311e29d
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 15:28:41 +0900

    uri 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/api/CommentController.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/api/CommentController.kt
@@ -2,9 +2,11 @@ package com.example.mediumspringwebmvc.domain.comment.api
 
 import com.example.mediumspringwebmvc.domain.comment.service.CreateComment
 import com.example.mediumspringwebmvc.domain.comment.service.DeleteComment
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/comments")
 class CommentController(
     private val createComment: CreateComment,
     private val deleteComment: DeleteComment
