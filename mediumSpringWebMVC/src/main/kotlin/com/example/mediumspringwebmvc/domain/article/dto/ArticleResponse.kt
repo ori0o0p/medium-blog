906a31fc375e531a0176c488c3bd81bb543fd36f
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 15:01:15 +0900

    comment 제거

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/dto/ArticleResponse.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/dto/ArticleResponse.kt
@@ -1,10 +1,7 @@
 package com.example.mediumspringwebmvc.domain.article.dto
 
-import com.example.mediumspringwebmvc.domain.comment.dto.CommentResponse
-
 data class ArticleResponse(
     val id: Long?,
     val title: String,
-    val description: String,
-    val comments: List<CommentResponse>
+    val description: String
 )
