75c213b3d70dcd01555388cc9a8ac41a70426a26
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 19:23:59 +0900

    jwt 생성

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/global/security/Tokenizer.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/global/security/Tokenizer.kt
@@ -1,4 +1,28 @@
 package com.example.mediumspringwebmvc.global.security
 
+import io.jsonwebtoken.*
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
 class Tokenizer {
+
+    companion object {
+        private const val SECRET = "secret"
+    }
+
+    fun generateToken(email: String): String {
+        val calender = Calendar.getInstance()
+        calender.add(Calendar.HOUR, 2)
+        val expiresAt = calender.time
+
+        return Jwts.builder()
+            .signWith(SignatureAlgorithm.HS256, SECRET)
+            .setSubject(email)
+            .setIssuedAt(Date())
+            .setExpiration(expiresAt)
+            .compact()
+    }
+
+
 }
