01a7b747d79227912d42beb0ca020804f4d4ca86
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 14:00:08 +0900

    article 객체 생성 로직 변경

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/CreateArticle.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/CreateArticle.kt
@@ -4,6 +4,7 @@ import com.example.mediumspringwebmvc.domain.article.dto.ArticleRequest
 import com.example.mediumspringwebmvc.domain.article.model.Article
 import com.example.mediumspringwebmvc.domain.article.repository.ArticleRepository
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
 @Service
 class CreateArticle(
@@ -13,7 +14,8 @@ class CreateArticle(
     fun execute(request: ArticleRequest) {
         val article = Article(
             title = request.title,
-            description = request.description)
+            description = request.description,
+            createdDate = LocalDate.now())
 
         articleRepository.save(article)
     }
