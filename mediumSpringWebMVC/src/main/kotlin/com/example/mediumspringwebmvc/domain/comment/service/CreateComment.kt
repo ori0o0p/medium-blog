bdceee2ddb340e433070f15febdd1f5a904a59b8
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 15:23:40 +0900

    댓글 생성 로직

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/service/CreateComment.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/service/CreateComment.kt
@@ -1,4 +1,29 @@
 package com.example.mediumspringwebmvc.domain.comment.service
 
-class CreateComment {
+import com.example.mediumspringwebmvc.domain.article.service.facade.FindArticle
+import com.example.mediumspringwebmvc.domain.comment.dto.CommentRequest
+import com.example.mediumspringwebmvc.domain.comment.model.Comment
+import com.example.mediumspringwebmvc.domain.comment.repository.CommentRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
+
+@Service
+class CreateComment(
+    private val commentRepository: CommentRepository,
+    private val findArticle: FindArticle
+) {
+
+    @Transactional
+    fun execute(request: CommentRequest) {
+        val article = findArticle.findById(request.articleId)
+
+        val comment = Comment(
+            content = request.content,
+            article = article,
+            createdDate = LocalDate.now())
+
+        commentRepository.save(comment)
+    }
+
 }
