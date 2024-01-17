c5c3637f0492e60909003d91e2c0743dbb10e70b
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 15:10:03 +0900

    jpa repository 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/repository/CommentRepository.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/repository/CommentRepository.kt
@@ -1,4 +1,7 @@
 package com.example.mediumspringwebmvc.domain.comment.repository
 
-interface CommentRepository {
+import com.example.mediumspringwebmvc.domain.comment.model.Comment
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CommentRepository: JpaRepository<Comment, Long> {
 }
