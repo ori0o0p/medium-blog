dbc869887d487bf88fe1434d5e7039f5b2773dd5
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 14:44:26 +0900

    create 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/api/ArticleController.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/api/ArticleController.kt
@@ -1,6 +1,9 @@
 package com.example.mediumspringwebmvc.domain.article.api
 
+import com.example.mediumspringwebmvc.domain.article.dto.ArticleRequest
 import com.example.mediumspringwebmvc.domain.article.service.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,8 +16,9 @@ class ArticleController(
     private val updateArticle: UpdateArticle,
     private val deleteArticle: DeleteArticle
 ) {
-    
-    fun create() {}
+
+    @PostMapping
+    fun create(@RequestBody request: ArticleRequest) = createArticle.execute(request)
 
     fun findAll() {}
 
