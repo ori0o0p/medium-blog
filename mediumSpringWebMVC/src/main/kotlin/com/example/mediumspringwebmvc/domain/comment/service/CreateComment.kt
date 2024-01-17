44b2048b74b3c163adf8e2ec95b15cac7e4638c9
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 22:39:06 +0900

    댓글에 유저 정보 저장

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/service/CreateComment.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/service/CreateComment.kt
@@ -4,6 +4,7 @@ import com.example.mediumspringwebmvc.domain.article.service.facade.FindArticle
 import com.example.mediumspringwebmvc.domain.comment.dto.CommentRequest
 import com.example.mediumspringwebmvc.domain.comment.model.Comment
 import com.example.mediumspringwebmvc.domain.comment.repository.CommentRepository
+import com.example.mediumspringwebmvc.domain.user.service.facade.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
@@ -11,16 +12,19 @@ import java.time.LocalDate
 @Service
 class CreateComment(
     private val commentRepository: CommentRepository,
-    private val findArticle: FindArticle
+    private val findArticle: FindArticle,
+    private val userFacade: UserFacade
 ) {
 
     @Transactional
     fun execute(request: CommentRequest) {
+        val user = userFacade.getUser()
         val article = findArticle.findById(request.articleId)
 
         val comment = Comment(
             content = request.content,
             article = article,
+            user = user,
             createdDate = LocalDate.now())
 
         commentRepository.save(comment)
