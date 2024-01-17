df17ad5299d1ad8b36ef08ffe2773933fac36320
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 18:56:36 +0900

    Like 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/like/model/Like.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/like/model/Like.kt
@@ -1,4 +1,22 @@
 package com.example.mediumspringwebmvc.domain.like.model
 
-class Like {
+import com.example.mediumspringwebmvc.domain.article.model.Article
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity
+class Like(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    val article: Article
+
+) {
 }
