80db34581b90aa92dd0a1b2bab49773ef7a5bfbc
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 14:03:33 +0900

    null 허용

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/dto/ArticleResponse.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/dto/ArticleResponse.kt
@@ -1,7 +1,7 @@
 package com.example.mediumspringwebmvc.domain.article.dto
 
 data class ArticleResponse(
-    val id: Long,
+    val id: Long?,
     val title: String,
     val description: String
 )
