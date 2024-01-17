8d69c305ac7284215f8785f8d40f6b16105fc939
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 22:37:32 +0900

    유저 정보 저장

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/CreateArticle.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/CreateArticle.kt
@@ -3,20 +3,25 @@ package com.example.mediumspringwebmvc.domain.article.service
 import com.example.mediumspringwebmvc.domain.article.dto.ArticleRequest
 import com.example.mediumspringwebmvc.domain.article.model.Article
 import com.example.mediumspringwebmvc.domain.article.repository.ArticleRepository
+import com.example.mediumspringwebmvc.domain.user.service.facade.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
 
 @Service
 class CreateArticle(
-    private val articleRepository: ArticleRepository
+    private val articleRepository: ArticleRepository,
+    private val userFacade: UserFacade
 ) {
 
     @Transactional
     fun execute(request: ArticleRequest) {
+        val user = userFacade.getUser()
+
         val article = Article(
             title = request.title,
             description = request.description,
+            user = user,
             createdDate = LocalDate.now(),
             updatedDate = LocalDate.now(),
             comments = ArrayList(),
