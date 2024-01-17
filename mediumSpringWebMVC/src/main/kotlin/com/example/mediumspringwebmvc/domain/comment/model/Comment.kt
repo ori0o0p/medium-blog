ff8738dd317a333a2248a88759eaa9d098d6367d
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 14:56:36 +0900

    comment model 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/model/Comment.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/model/Comment.kt
@@ -1,4 +1,24 @@
 package com.example.mediumspringwebmvc.domain.comment.model
 
-class Comment {
+import com.example.mediumspringwebmvc.domain.article.model.Article
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity
+class Comment(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    var content: String,
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    val article: Article
+
+) {
 }
