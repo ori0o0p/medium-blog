fea36a17ce6acd53f906d1fc872d2a1db7c5176d
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 22:28:30 +0900

    tokenizer 코드 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/global/security/Tokenizer.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/global/security/Tokenizer.kt
@@ -1,6 +1,9 @@
 package com.example.mediumspringwebmvc.global.security
 
 import io.jsonwebtoken.*
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -26,12 +29,25 @@ class Tokenizer {
 
     fun verifyToken(token: String?): Boolean {
         try {
-            Jwts.parser().setSigningKey(SECRET)
-                .parseClaimsJws(token)
+            parse(token)
             return true
         } catch (e: Exception) {
             return false
         }
+    }
+
+    private fun parse(token: String?): Jws<Claims> {
+        return Jwts.parser().setSigningKey(SECRET)
+            .parseClaimsJws(token)
+    }
+
+    fun getAuthentication(token: String?): Authentication {
+        val claims =
+    }
+
+    private fun createUserByClaims(claims: Claims): UserDetails {
+        val subject = claims.subject
+        return User(subject, "", Collections.emptyList())
     }
 
 }
