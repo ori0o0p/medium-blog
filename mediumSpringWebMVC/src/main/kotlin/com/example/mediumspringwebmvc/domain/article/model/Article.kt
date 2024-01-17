59a85b90f49b4b1ac1f4c4d9f624731948ba9fa4
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 14:56:44 +0900

    매핑관계 설정

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/model/Article.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/model/Article.kt
@@ -2,10 +2,13 @@ package com.example.mediumspringwebmvc.domain.article.model
 
 import com.example.mediumspringwebmvc.domain.article.dto.ArticleRequest
 import com.example.mediumspringwebmvc.domain.article.dto.ArticleResponse
+import com.example.mediumspringwebmvc.domain.comment.model.Comment
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import java.time.LocalDate
 
 @Entity
@@ -20,7 +23,10 @@ class Article(
 
     val createdDate: LocalDate,
 
-    var updatedDate: LocalDate
+    var updatedDate: LocalDate,
+
+    @OneToMany(mappedBy = "article", cascade = [CascadeType.ALL])
+    var comments: List<Comment>
 
 ) {
 
