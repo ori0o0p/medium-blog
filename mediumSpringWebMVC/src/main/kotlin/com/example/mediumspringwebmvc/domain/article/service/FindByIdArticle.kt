62c25987196b18d1ed2e4e58f2c9275558babbb9
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 14:26:53 +0900

    로직 수정

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/FindByIdArticle.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/FindByIdArticle.kt
@@ -2,18 +2,18 @@ package com.example.mediumspringwebmvc.domain.article.service
 
 import com.example.mediumspringwebmvc.domain.article.dto.ArticleResponse
 import com.example.mediumspringwebmvc.domain.article.repository.ArticleRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import kotlin.jvm.optionals.getOrNull
 
 @Service
 class FindByIdArticle(
     private val articleRepository: ArticleRepository
 ) {
 
-    fun execute(id: Long): ArticleResponse? {
-        val article = articleRepository.findById(id).getOrNull()
+    fun execute(id: Long): ArticleResponse {
+        val article = articleRepository.findByIdOrNull(id) ?: throw RuntimeException("게시물을 찾지 못함")
 
-        return article?.toArticleResponse()
+        return article.toArticleResponse()
     }
 
 }
