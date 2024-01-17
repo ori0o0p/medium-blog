92fd175c232d2b42f56f0bee938425d2ed858f3c
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 13:57:42 +0900

    article create service 작성

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/CreateArticle.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/CreateArticle.kt
@@ -1,4 +1,21 @@
 package com.example.mediumspringwebmvc.domain.article.service
 
-class CreateArticle {
+import com.example.mediumspringwebmvc.domain.article.dto.ArticleRequest
+import com.example.mediumspringwebmvc.domain.article.model.Article
+import com.example.mediumspringwebmvc.domain.article.repository.ArticleRepository
+import org.springframework.stereotype.Service
+
+@Service
+class CreateArticle(
+    private val articleRepository: ArticleRepository
+) {
+
+    fun execute(request: ArticleRequest) {
+        val article = Article(
+            title = request.title,
+            description = request.description)
+
+        articleRepository.save(article)
+    }
+
 }
