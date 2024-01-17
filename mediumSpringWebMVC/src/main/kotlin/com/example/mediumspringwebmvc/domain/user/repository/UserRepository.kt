d1f13416c6ce2349db5f145ada393adaa1320237
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 22:36:18 +0900

    find by email 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/user/repository/UserRepository.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/user/repository/UserRepository.kt
@@ -4,4 +4,6 @@ import com.example.mediumspringwebmvc.domain.user.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository: JpaRepository<User, Long> {
+
+    fun findByEmail(email: String): User?
 }
