d876720efcbf9f4305a31e9cd3e70dd56a8ed7f9
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 22:16:46 +0900

    filter token

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/global/security/JwtFilter.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/global/security/JwtFilter.kt
@@ -1,4 +1,36 @@
 package com.example.mediumspringwebmvc.global.security
 
-class JwtFilter {
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.util.StringUtils
+import org.springframework.web.filter.OncePerRequestFilter
+
+class JwtFilter(
+    private val tokenizer: Tokenizer
+): OncePerRequestFilter {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token = extractToken(request)
+
+        if (StringUtils.hasText(token) && ) {
+            
+        }
+    }
+
+    private fun extractToken(request: HttpServletRequest): String? {
+        val token = request.getHeader(HttpHeaders.AUTHORIZATION)
+
+        if (token.startsWith("Bearer ")) {
+            return token.substring(7)
+        }
+
+        return null
+    }
+
 }
