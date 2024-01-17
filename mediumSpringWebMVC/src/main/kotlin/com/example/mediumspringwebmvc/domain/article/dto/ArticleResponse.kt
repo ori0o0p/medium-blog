3d90339f44ee72b8187b15b688e7cc854bc9533b
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 14:01:45 +0900

    dto 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/dto/ArticleResponse.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/dto/ArticleResponse.kt
@@ -1,3 +1,7 @@
 package com.example.mediumspringwebmvc.domain.article.dto
 
-data class ArticleResponse()
+data class ArticleResponse(
+    val id: Long,
+    val title: String,
+    val description: String
+)
