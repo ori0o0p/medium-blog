453599ab3c1c0de8e41ded5193b05dbe126925ad
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 14:30:07 +0900

    find by id 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/facade/FindArticle.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/facade/FindArticle.kt
@@ -1,4 +1,16 @@
 package com.example.mediumspringwebmvc.domain.article.service.facade
 
-class FindArticle {
+import com.example.mediumspringwebmvc.domain.article.model.Article
+import com.example.mediumspringwebmvc.domain.article.repository.ArticleRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class FindArticle(
+    private val articleRepository: ArticleRepository
+) {
+
+    fun findById(id: Long): Article =
+        articleRepository.findByIdOrNull(id) ?: throw RuntimeException("게시물을 찾지 못함")
+
 }
