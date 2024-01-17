d3461fea1c0cecb516ca577e6324c3e44a3ac69c
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 22:40:11 +0900

    게시물 response dto 변경

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/dto/ArticleResponse.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/dto/ArticleResponse.kt
@@ -1,7 +1,10 @@
 package com.example.mediumspringwebmvc.domain.article.dto
 
+import com.example.mediumspringwebmvc.domain.user.model.User
+
 data class ArticleResponse(
     val id: Long?,
+    val user: User,
     val title: String,
     val description: String
 )
