829c0daf152629af2f74b626dadf9064a5c2ba08
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 14:19:13 +0900

    게시물 단일 조회

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/FindByIdArticle.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/article/service/FindByIdArticle.kt
@@ -1,4 +1,19 @@
 package com.example.mediumspringwebmvc.domain.article.service
 
-class FindByIdArticle {
+import com.example.mediumspringwebmvc.domain.article.dto.ArticleResponse
+import com.example.mediumspringwebmvc.domain.article.repository.ArticleRepository
+import org.springframework.stereotype.Service
+import kotlin.jvm.optionals.getOrNull
+
+@Service
+class FindByIdArticle(
+    private val articleRepository: ArticleRepository
+) {
+
+    fun execute(id: Long): ArticleResponse? {
+        val article = articleRepository.findById(id).getOrNull()
+
+        return article?.toArticleResponse()
+    }
+
 }
