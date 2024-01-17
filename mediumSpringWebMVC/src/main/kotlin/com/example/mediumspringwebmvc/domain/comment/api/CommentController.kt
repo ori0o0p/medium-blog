c55e1b457cb0c1a9584ec8aec00b2a88146ed955
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 15:28:14 +0900

    service 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/api/CommentController.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/api/CommentController.kt
@@ -1,4 +1,12 @@
 package com.example.mediumspringwebmvc.domain.comment.api
 
-class CommentController {
+import com.example.mediumspringwebmvc.domain.comment.service.CreateComment
+import com.example.mediumspringwebmvc.domain.comment.service.DeleteComment
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CommentController(
+    private val createComment: CreateComment,
+    private val deleteComment: DeleteComment
+) {
 }
