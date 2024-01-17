d9cde360f73724b0eda5eb3e442726e6f869a750
101 <tmddnjs001133@dsm.hs.kr>
2024-01-17 15:27:09 +0900

    댓글 삭제 로직  추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/service/DeleteComment.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/comment/service/DeleteComment.kt
@@ -1,4 +1,20 @@
 package com.example.mediumspringwebmvc.domain.comment.service
 
-class DeleteComment {
+import com.example.mediumspringwebmvc.domain.comment.repository.CommentRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class DeleteComment(
+    private val commentRepository: CommentRepository
+) {
+
+    @Transactional
+    fun execute(id: Long) {
+        val comment = commentRepository.findByIdOrNull(id) ?: throw RuntimeException("댓글을 찾지 못함")
+
+        commentRepository.delete(comment)
+    }
+
 }
