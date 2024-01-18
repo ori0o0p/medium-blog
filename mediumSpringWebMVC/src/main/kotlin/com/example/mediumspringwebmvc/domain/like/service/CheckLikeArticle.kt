468403bf0ebdce1d941c2988d17928712b8595ee
101 <tmddnjs001133@dsm.hs.kr>
2024-01-18 14:20:23 +0900

    추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/like/service/CheckLikeArticle.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/like/service/CheckLikeArticle.kt
@@ -1,4 +1,24 @@
 package com.example.mediumspringwebmvc.domain.like.service
 
-class CheckLikeArticle {
+import com.example.mediumspringwebmvc.domain.article.service.facade.FindArticle
+import com.example.mediumspringwebmvc.domain.like.repository.LikeRepository
+import com.example.mediumspringwebmvc.domain.user.service.facade.UserFacade
+import org.springframework.stereotype.Service
+
+@Service
+class CheckLikeArticle(
+    private val likeRepository: LikeRepository,
+    private val findArticle: FindArticle,
+    private val userFacade: UserFacade
+) {
+
+    fun execute(id: Long): Boolean {
+        val user = userFacade.getUser()
+        val article = findArticle.findById(id)
+
+        likeRepository.findByArticleAndUser(article, user) ?: return false
+
+        return true
+    }
+
 }
