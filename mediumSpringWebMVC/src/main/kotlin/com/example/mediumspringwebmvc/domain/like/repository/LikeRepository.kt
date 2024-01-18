48ab8fc45b51001982be6cc2826e262a946ca01b
101 <tmddnjs001133@dsm.hs.kr>
2024-01-18 14:20:05 +0900

    find by article, user 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/like/repository/LikeRepository.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/like/repository/LikeRepository.kt
@@ -1,7 +1,11 @@
 package com.example.mediumspringwebmvc.domain.like.repository
 
+import com.example.mediumspringwebmvc.domain.article.model.Article
 import com.example.mediumspringwebmvc.domain.like.model.Like
+import com.example.mediumspringwebmvc.domain.user.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface LikeRepository: JpaRepository<Like, Long> {
+
+    fun findByArticleAndUser(article: Article, user: User): Like?
 }
