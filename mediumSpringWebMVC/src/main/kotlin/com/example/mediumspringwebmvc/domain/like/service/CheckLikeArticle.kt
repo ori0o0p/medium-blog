6ad6ce7168e8ce906a8029300d6827a554c4226a
101 <tmddnjs001133@dsm.hs.kr>
2024-01-18 15:29:28 +0900

    어노테이션 추가

--- mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/like/service/CheckLikeArticle.kt
+++ mediumSpringWebMVC/src/main/kotlin/com/example/mediumspringwebmvc/domain/like/service/CheckLikeArticle.kt
@@ -2,12 +2,14 @@ package com.example.mediumspringwebmvc.domain.like.service
 
 import com.example.mediumspringwebmvc.domain.like.service.facade.FindLike
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CheckLikeArticle(
     private val findLike: FindLike
 ) {
 
+    @Transactional(readOnly = true)
     fun execute(id: Long): Boolean {
         findLike.findById(id) ?: return false
 
