4baa26e245090449f3b947da73ace42bb213cce6
101 <tmddnjs001133@dsm.hs.kr>
2024-02-20 01:12:32 +0900

    update

--- studyTDD/src/main/java/com/example/studytdd/config/WebClientConfig.java
+++ studyTDD/src/main/java/com/example/studytdd/config/WebClientConfig.java
@@ -1,2 +1,7 @@
-package com.example.studytdd.config;public class WebClientConfig {
+package com.example.studytdd.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebClientConfig {
 }
