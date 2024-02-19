fc43ab57b5add96fe8c0bb6a35858bc06d66f7a6
101 <tmddnjs001133@dsm.hs.kr>
2024-02-20 01:08:22 +0900

    study tdd 시작

---
+++ studyTDD/src/test/java/com/example/studytdd/StudyTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studytdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
