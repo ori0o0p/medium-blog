593e670252576b144ee2ca5e05d2fe626f23f6df
101 <tmddnjs001133@dsm.hs.kr>
2024-02-20 12:19:01 +0900

    test 추가

--- studyTDD/src/test/java/com/example/studytdd/StudyTddApplicationTests.java
+++ studyTDD/src/test/java/com/example/studytdd/StudyTddApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.studytdd;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+class WebClientConfig {
+
+    private String id = System.getenv().get("NAVER_ID");
+
+    private String secret = System.getenv().get("NAVER_SECRET");
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("https://openapi.naver.com/v1/search/local.json")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("X-Naver-Client-Id", id)
+                .defaultHeader("X-Naver-Client-Secret", secret)
+                .build();
+    }
+
+}
+
 
 @SpringBootTest
-class StudyTddApplicationTests {
+public class StudyTddApplicationTests {
+    @Autowired
+    private WebClient webCilent;
 
     @Test
-    void contextLoads() {
+    void testGet() {
+        String response = webCilent.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("query", "마트")
+                        .queryParam("display", 5)
+                        .queryParam("start", 1)
+                        .queryParam("sort", "random")
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println(response);
     }
 
 }
