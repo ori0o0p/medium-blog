319a3a7c3c6dae0a1a24f2630c943cf4546421da
101 <tmddnjs001133@dsm.hs.kr>
2024-02-20 12:33:30 +0900

    WebClientConfig를 NaverSearchAPI 이너클래스로 이동

--- studyTDD/src/test/java/com/example/studytdd/NaverSearchAPI.java
+++ studyTDD/src/test/java/com/example/studytdd/NaverSearchAPI.java
@@ -1,8 +1,31 @@
 package com.example.studytdd;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+class WebClientConfig {
+
+    private String id = System.getenv().get("NAVER_ID");
+
+    private String secret = System.getenv().get("NAVER_SECRET");
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("https://openapi.naver.com/v1/search/local.json")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("X-Naver-Client-Id", id)
+                .defaultHeader("X-Naver-Client-Secret", secret)
+                .build();
+    }
+
+}
 
 @Component
 public class NaverSearchAPI {
--- studyTDD/src/test/java/com/example/studytdd/StudyTddApplicationTests.java
+++ studyTDD/src/test/java/com/example/studytdd/StudyTddApplicationTests.java
@@ -3,37 +3,9 @@ package com.example.studytdd;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.util.Assert;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
-
-@Configuration
-class WebClientConfig {
-
-    private String id = System.getenv().get("NAVER_ID");
-
-    private String secret = System.getenv().get("NAVER_SECRET");
-
-    @Bean
-    public WebClient webClient() {
-        return WebClient.builder()
-                .baseUrl("https://openapi.naver.com/v1/search/local.json")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .defaultHeader("X-Naver-Client-Id", id)
-                .defaultHeader("X-Naver-Client-Secret", secret)
-                .build();
-    }
-
-}
-
 
 @SpringBootTest
 public class StudyTddApplicationTests {
