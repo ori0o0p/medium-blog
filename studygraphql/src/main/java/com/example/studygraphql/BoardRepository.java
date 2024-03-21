91e16d4015c010773b2b85e5347698608700b368
101 <tmddnjs001133@dsm.hs.kr>
2024-03-21 20:22:33 +0900

    레포지토리 작성

--- studygraphql/src/main/java/com/example/studygraphql/BoardRepository.java
+++ studygraphql/src/main/java/com/example/studygraphql/BoardRepository.java
@@ -1,2 +1,7 @@
-package com.example.studygraphql;public interface BoardRepository {
+package com.example.studygraphql;
+
+import com.example.studygraphql.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
 }
