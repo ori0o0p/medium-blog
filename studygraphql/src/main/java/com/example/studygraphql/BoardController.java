b81a1e6aef8af95de0e9932ddbfea4813ed71987
101 <tmddnjs001133@dsm.hs.kr>
2024-03-21 21:47:35 +0900

    refactor BoardController.java

--- studygraphql/src/main/java/com/example/studygraphql/BoardController.java
+++ studygraphql/src/main/java/com/example/studygraphql/BoardController.java
@@ -3,7 +3,6 @@ package com.example.studygraphql;
 import com.example.studygraphql.model.Board;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
