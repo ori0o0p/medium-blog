69baa0001d9e939b69419fa62b99f8495f876e2a
101 <tmddnjs001133@dsm.hs.kr>
2024-03-21 20:22:41 +0900

    컨트롤러 작성

--- studygraphql/src/main/java/com/example/studygraphql/BoardController.java
+++ studygraphql/src/main/java/com/example/studygraphql/BoardController.java
@@ -1,2 +1,33 @@
-package com.example.studygraphql;public class BoardController {
+package com.example.studygraphql;
+
+import com.example.studygraphql.model.Board;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardRepository boardRepository;
+
+    @SchemaMapping(typeName = "Query", field = "findById")
+    public Board findById(Long id) {
+        return boardRepository.findById(id)
+                .orElse(null);
+    }
+
+    @SchemaMapping(typeName = "Query", field = "findAll")
+    public List<Board> findAll() {
+        return boardRepository.findAll();
+    }
+
+    @SchemaMapping(typeName = "Mutation", field = "save")
+    public Board save(@Argument("title") String title, @Argument("content") String content) {
+        return boardRepository.save(new Board(title, content));
+    }
+
 }
