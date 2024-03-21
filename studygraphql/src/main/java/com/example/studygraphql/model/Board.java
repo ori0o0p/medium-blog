2c8d91ccef7c71ab5bae3ca72fc3b85a98a8bf22
101 <tmddnjs001133@dsm.hs.kr>
2024-03-21 20:22:27 +0900

    엔티티 작성

--- studygraphql/src/main/java/com/example/studygraphql/model/Board.java
+++ studygraphql/src/main/java/com/example/studygraphql/model/Board.java
@@ -1,2 +1,27 @@
-package com.example.studygraphql.model;public record Board() {
+package com.example.studygraphql.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Entity
+@Table(name = "board")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    public Board() {}
+
+    public Board(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    // Getters and setters
 }
